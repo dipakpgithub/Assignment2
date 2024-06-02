25e57d3f461ac20e87c667b970a282a99005c47f
dipak <dipak@DIPAK>
2024-06-02 17:50:58 -0400

    Product class creates

---
+++ Assignment2/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+
+    // Producat class
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+
+        public Product(int productId, string productName, decimal price, int stock) => (ProductId, ProductName, Price, Stock) = (productId, productName, price, stock);
+
+
+        // Method for Increase stock
+        public void IncreaseStock(int amount)
+        {
+            if (amount > 0) Stock += amount;
+
+        }
+        // Method for descrease stock
+        public void DecreaseStock(int amount)
+        {
+            if (amount > 0 && amount <= Stock) Stock -= amount;
+
+        }
+
+    }
+}
